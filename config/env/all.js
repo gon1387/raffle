c0d59372c4393fb14d51c31d17f265f279ce3194
Carlo Gonzales <carloagonzales@gmail.com>
2014-12-01 16:28:50 +0800

    add PapaParse js file on client html

--- config/env/all.js
+++ config/env/all.js
@@ -28,7 +28,8 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',	
-				'public/lib/angular-csv-import/dist/angular-csv-import.js'
+				'public/lib/angular-csv-import/dist/angular-csv-import.js',
+				'public/lib/papaparse/papaparse.js'
 			]
 		},
 		css: [
